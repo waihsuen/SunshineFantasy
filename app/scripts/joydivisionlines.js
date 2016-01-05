e1264c8f20368d526c717e24ace21013991f7693
Noel Chan <noel.chanwh@ymail.com>
2016-01-06 00:37:30 +0800

    Joy

--- app/scripts/joydivisionlines.js
+++ app/scripts/joydivisionlines.js
@@ -1,284 +1,191 @@
-
-if ( !window.requestAnimationFrame ) {
-	window.requestAnimationFrame = ( function() {
-		return window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame ||
-		window.oRequestAnimationFrame ||
-		window.msRequestAnimationFrame ||
-		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
-			window.setTimeout( callback, 1000 / 60 );
-		};
-	} )();
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (function () {
+        return window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
 }
 
+var _lineGroup,
+    _lineHolder,
+    camera,
+    scene,
+    renderer,
+    _material,
+    _inputImage,
+    _canvas,
+    context,
+    _imageWidth,
+    _imageHeight,
+    _stageWidth,
+    _stageHeight;
 
-//var renderer = new THREE.WebGLRenderer();
-//// RENDER
-//renderer.setClearColor(0xdddddd);
-//renderer.setSize(window.innerWidth, window.innerHeight);
-//renderer.shadowMapEnabled = true
-//renderer.shadowMapSoft = true;
-//document.body.appendChild(renderer.domElement);
-
-//VARS
-var _stage,
-_lineGroup,
-_lineHolder,
-_stats,
-_camera,
-_scene,
-_renderer,
-_mouseX = 0,
-_mouseY = 0,
-_material,
-_gui,
-_inputImage,
-_stageCenterX,
-_stageCenterY,
-_canvas,
-_context,
-_imageWidth,
-_imageHeight,
-_stageWidth,
-_stageHeight,
-_enableMouseMove = false,
-
-//VARS ACCESSIBLE BY GUI
-_guiOptions  = {
-	stageSize:	 	0.8,
-	scale:	 		2.0,
-	scanStep: 		5,
-	lineThickness:	3.0,
-	opacity: 		1.0,
-	depth: 			100,
-	autoRotate: 	false
-};
+var imageArray = ['images/a.jpg', 'images/b.jpg', 'images/c.jpg', 'images/d.jpg', 'images/e.jpg', 'images/f.jpg', 'images/g.jpg'],
+    imageCounter = 0;
 
 
-// Init GUI
-//var datGUI = new dat.GUI();
+$(document).ready(function () {
 
-//dat.GUI.autoPlace = false;
-//var _gui = new dat.GUI();
-//document.getElementById('controls-container').appendChild( _gui.domElement );
-//
-////_gui = new DAT.GUI();
-//_gui.add(_guiOptions, 'stageSize',.2,1,.1).onChange(doLayout).name('Stage Size');
-//_gui.add(_guiOptions, 'scale', 0.1, 10,0.1).listen().name('Scale');
-//_gui.add(_guiOptions, 'scanStep', 1, 20,1).onChange( createLines ).name('Line Separation');
-//_gui.add(_guiOptions, 'lineThickness', 0.1, 10,0.1).onChange( updateMaterial ).name('Line Thickness');
-//_gui.add(_guiOptions, 'depth', 0, 300,1).onChange( createLines ).name('Max Line Depth');
-//_gui.add(_guiOptions, 'opacity', 0, 1,0.1).onChange( updateMaterial ).name('Brightness');
-//_gui.add(this, 'saveImage').name('Save Image');
+    var SCREEN_WIDTH, SCREEN_HEIGHT;
+    //
+    _canvas = document.createElement('canvas');
+    context = _canvas.getContext('2d');
 
-/**
- * Init page
- */
-$(document).ready( function() {
+    $(window).resize(function () {
+        SCREEN_WIDTH = window.innerWidth;
+        SCREEN_HEIGHT = window.innerHeight;
 
-	$(window).bind('resize', doLayout);
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+        camera.updateProjectionMatrix();
 
-	_inputImage = new Image();
-	_inputImage.src = ("images/a.jpg");
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    });
 
-	_inputImage.onload = function() {
-		// load image into canvas pixels
-        _imageWidth = _inputImage.width;
-        _imageHeight = _inputImage.height;
-        _canvas	= document.createElement('canvas');
-        _canvas.width = _imageWidth
-        _canvas.height = _imageHeight;
-        _context = _canvas.getContext('2d');
-        _context.drawImage(_inputImage, 0, 0);
-        _pixels	= _context.getImageData(0,0,_imageWidth,_imageHeight).data;
-
-        createLines();
-	};
-
-	// stop the user getting a text cursor
-	document.onselectstart = function() {
-		return false;
-	};
-	_stage = document.getElementById("stage");
-
-	
-	//init mouse listeners
-	$("#stage").mousemove( onMouseMove);
-	$(window).mousewheel( onMouseWheel);
-	
-	$(window).mousedown( function() {
-		_enableMouseMove = true;
-	});
-	$(window).mouseup( function() {
-		_enableMouseMove = false;
-	});
-
-	doLayout();
-    
-	initWebGL();
-
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    //
+    //camera = new THREE.PerspectiveCamera(75, 16/9, 1, 3000);
+    camera.position.z = 0;
+    //camera.position.x = camera.position.y = camera.position.z = 10;
+    //camera.position.y = 100;
+    camera.lookAt(scene.position);
+    //
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
+    //
+    document.body.appendChild(renderer.domElement);
+    //
+    loadImageAndReadyScene();
+    animate();
 });
-function initWebGL() {
 
-	//init camera
-	_camera = new THREE.Camera(75, 16/9, 1, 3000);
-	_camera.position.z = -1000;
-	_scene = new THREE.Scene();
 
-	//init renderer
-	_renderer = new THREE.WebGLRenderer({
-		antialias: true,
-		clearAlpha: 1,
-		sortObjects: false,
-		sortElements: false
-	});
-    
-    _renderer.setSize(window.innerWidth, window.innerHeight);
-
-	_lineHolder = new THREE.Object3D();
-	_scene.addObject(_lineHolder);
-
-	animate();
-}
-
-/**
- * Create Lines from image
- */
 function createLines() {
+    if (_lineHolder)
+        scene.remove(_lineHolder);
+    
+    _lineHolder = new THREE.Object3D();
+    scene.add(_lineHolder);
+    _lineHolder.rotation.x = Math.PI / 2;
+    _lineHolder.rotation.y = 0;
+    _lineHolder.rotation.z = 0;
 
-	document.body.appendChild(_renderer.domElement);
+    var x = 0,
+        y = 0;
 
-	var x = 0, y = 0;
+    if (_lineGroup)
+        scene.remove(_lineGroup);
 
-	if (_lineGroup)
-		_scene.removeObject(_lineGroup);
+    _lineGroup = new THREE.Object3D();
 
-	_lineGroup = new THREE.Object3D();
+    _material = new THREE.LineBasicMaterial({
+        color: 0xffffff,
+        opacity: 1.0,
+        linewidth: 0.5,
+        blending: THREE.AdditiveBlending,
+        depthTest: false,
+        vertexColors: true
+    });
 
-	_material = new THREE.LineBasicMaterial({
-		color: 0xffffff,
-		opacity: _guiOptions.opacity,
-		linewidth: _guiOptions.lineThickness,
-		blending: THREE.AdditiveBlending,
-		depthTest: false,
-		vertexColors: true
-	} );
+    var steps = 6,
+        depth = 80;
 
-	// go through the image pixels
-	for(y = 0; y < _imageHeight; y+= _guiOptions.scanStep) {
-		var geometry = new THREE.Geometry();
-		for(x = 0; x < _imageWidth ; x+= _guiOptions.scanStep) {
-			var color = new THREE.Color(getColor(x, y));
-			var brightness = getBrightness(color);
-			var posn = new THREE.Vector3(x -_imageWidth/2,y - _imageHeight/2, -brightness * _guiOptions.depth + _guiOptions.depth/2);
-			geometry.vertices.push(new THREE.Vertex(posn));
-			geometry.colors.push(color);
-		}
-		//add a line
-		var line = new THREE.Line(geometry, _material );
-		_lineGroup.addChild(line);
-	}
+    for (y = 0; y < _imageHeight; y += steps) {
+        var geometry = new THREE.Geometry();
+        for (x = 0; x < _imageWidth; x += steps) {
+            var color = new THREE.Color(getColor(x, y));
+            var brightness = getBrightness(color);
+            var posn = new THREE.Vector3(x - _imageWidth / 2, y - _imageHeight / 2, -brightness * depth + depth / 2);
+            geometry.vertices.push(posn);
+            geometry.colors.push(color);
+        }
+        //add a line
+        var line = new THREE.Line(geometry, _material);
+        _lineGroup.add(line);
+    }
 
-	_lineHolder.addChild(_lineGroup);
+    _lineHolder.add(_lineGroup);
 }
 
-function updateMaterial() {
-	if (_material) {
-		_material.opacity = _guiOptions.opacity;
-		_material.linewidth = _guiOptions.lineThickness;
-	}
-}
-
-function onMouseMove(event) {
-	if (_enableMouseMove) {
-		_mouseX = event.pageX - _stageCenterX;
-		_mouseY = event.pageY - _stageCenterY;
-	}
-}
-
-function onMouseWheel(e,delta) {
-	_guiOptions.scale += delta * 0.1;
-	//limit
-	_guiOptions.scale = Math.max(_guiOptions.scale, .1);
-	_guiOptions.scale = Math.min(_guiOptions.scale, 10);
-}
-
-function onKeyDown(evt) {
-	//save on 'S' key
-	if (event.keyCode == '83') {
-		saveImage();
-	}
-}
-
-function animate() {
-	requestAnimationFrame(animate);
-	render();
+function animate(time) {
+    requestAnimationFrame(animate);
+    render();
+    TWEEN.update(time);
 }
 
 function render() {
-
-	_lineHolder.scale = new THREE.Vector3(_guiOptions.scale,_guiOptions.scale, _guiOptions.scale);
-
-	var xrot = _mouseX/_stageWidth * Math.PI*2 + Math.PI;
-	var yrot = _mouseY/_stageHeight* Math.PI*2 + Math.PI;
-
-	_lineHolder.rotation.x += (-yrot - _lineHolder.rotation.x) * 0.3;
-	_lineHolder.rotation.y += (xrot - _lineHolder.rotation.y) * 0.3;
-
-	_renderer.render(_scene, _camera);
+    renderer.render(scene, camera);
 }
 
-function doLayout() {
+function startAnimation() {
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 0;
+    camera.zoom = 1;
+    
+    new TWEEN.Tween(camera.position).to({
+        x: 10,
+        y: 10,
+        z: 1000
+    }, 5000).start();
 
-	var winHeight, winWidth, controlsWidth, containerWidth;
-
-	//get dims
-	winHeight = window.innerHeight ? window.innerHeight : $(window).height();
-	winWidth = window.innerWidth ? window.innerWidth : $(window).width();
-
-
-	//set stage size as fraction of window size
-	//use letterbox dimensions unless 100%
-	_stageWidth = parseInt(winWidth);
-	_stageHeight = parseInt(winHeight) * 9 / 16;
-
-//	if (_guiOptions.stageSize === 1) {
-//		_stageHeight = $('#container').outerHeight();
-//	}
-//	$('#stage').width(parseInt(winHeight));
-//	$('#stage').height(parseInt(winWidth));
-
-	//Center stage div inside window
-//	$('#stage').css({
-//		left: Math.max((containerWidth - _stageWidth)/2 + controlsWidth,controlsWidth),
-//		top: (winHeight -_stageHeight)/2,
-//		visibility:"visible"
-//	});
-
-	//set webgl size
-	if (_renderer) {
-		_renderer.setSize(window.innerWidth, window.innerHeight);
-		_camera.aspect = window.innerWidth / window.innerHeight;
-		_camera.updateProjectionMatrix();
-	}
-
-	_stageCenterX = window.innerWidth / 2;
-	_stageCenterY = window.innerHeight / 2
+    new TWEEN.Tween(_lineHolder.rotation).to({
+        x: Math.PI,
+        y: 0,
+        z: 0
+    }, 2000).delay(3400).start().onComplete(function () {
+        setTimeout(loadImageAndReadyScene, 3000);
+    });
 }
 
-// Returns a hexidecimal color for a given pixel in the pixel array.
+function loadImageAndReadyScene() {
+    _inputImage = new Image();
+    _inputImage.src = (imageArray[imageCounter]);
+
+    _inputImage.onload = function () {
+        _imageWidth = _inputImage.width;
+        _imageHeight = _inputImage.height;
+
+        _canvas.width = _imageWidth
+        _canvas.height = _imageHeight;
+
+        context.drawImage(_inputImage, 0, 0);
+        _pixels = context.getImageData(0, 0, _imageWidth, _imageHeight).data;
+        createLines();
+        startAnimation();
+        
+        //
+        $(window).trigger('resize');
+        
+        if (imageCounter < imageArray.length-1) {
+            imageCounter++;
+        } else {
+            imageCounter = 0;
+        }
+    };
+}
+
 function getColor(x, y) {
-	var base = (Math.floor(y) * _imageWidth + Math.floor(x)) * 4;
-	var c = {
-		r: _pixels[base + 0],
-		g: _pixels[base + 1],
-		b: _pixels[base + 2],
-		a: _pixels[base + 3]
-	};
-	return (c.r << 16) + (c.g << 8) + c.b;
+    var base = (Math.floor(y) * _imageWidth + Math.floor(x)) * 4;
+    var c = {
+        r: _pixels[base + 0],
+        g: _pixels[base + 1],
+        b: _pixels[base + 2],
+        a: _pixels[base + 3]
+    };
+    return (c.r << 16) + (c.g << 8) + c.b;
 };
 
-//return pixel brightness between 0 and 1 based on human perceptual bias
 function getBrightness(c) {
-	return ( 0.34 * c.r + 0.5 * c.g + 0.16 * c.b );
+    return (0.34 * c.r + 0.5 * c.g + 0.16 * c.b);
 };
